f804fba7585c537aa919d7c16e2ddd5a7121ad3d
Quinn Johnston <114368726+sqj0002@users.noreply.github.com>
2023-05-01 18:19:06 -0500

    Update Download_FastQC.sh

--- Download_FastQC.sh
+++ Download_FastQC.sh
@@ -1,25 +1,29 @@
 #! /bin/bash
 
-######### FunGen Course Instructions ############
-## Purpose: The purpose of this script is to 
-## 	learn to make scratch directory
-## 	learn to define variables
-## 	download data from NCBI SRA using the SRAtoolkit and the SRA run IDs: https://www.ncbi.nlm.nih.gov/sra/docs/sradownload/
-## 	use FASTQC to evaluate the quality of the data: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
-## Download from SRA: Input Data: NA
-## 			Output: Downloaded read files, R1 and R2 files for each sample if paired-end data (FASTQ)
-## FASTQC 	InPut: Downloade SRA files .fastq
-##		Output: is a folder for each file. The last line of this script will make a tarball of the output directory to bring back to your computer
+######### Script 1: Download SRA (Sequence Read Archives) files and Perform Preliminary FastQc ############
+## Purpose: The purpose of this script is to
+##	make a temporary scratch directory to read in raw data SRA sequences 
+##	download data from NCBI SRA using the SRAtoolkit and the SRA run IDs: https://www.ncbi.nlm.nih.gov/sra/docs/sradownload/
+##	use FASTQC to evaluate the quality of the data: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
+
+## Download from SRA Pipepline: 
+##  Input Data: N/A
+##  Output: Downloaded read files, R1 and R2 files for each sample if paired-end data (FASTQ)
+
+## FASTQC Pipeline:
+##	Input: Downloaded SRA files .fastq
+##  Output: Folder for each input .fastq SRA file; The last line of this script will make a tarball of the output directory (PreCleanQuality) to bring back to your computer
+
 ## For running the script on the Alabama Super Computer.
 ##	For more information: https://hpcdocs.asc.edu/content/slurm-queue-system
-## 	After you have this script in your home directory and you have made it executable using  "chmod +x [script name]", 
-## 	then run the script by using "run_script [script name]"
-## 	suggested paramenters are below to submit this script.
-## 		queue: class
-##		core: 1
-##		time limit (HH:MM:SS): 04:00:00 
-##		Memory: 4gb
-##		run on dmc
+##	After you have this script in your ASC directory, Download_qc, and you have made it executable using "chmod +x [script name]",
+##	then run the script by using "run_script [script name]"
+##	suggested paramenters are below to submit this script.
+##              queue: class
+##              core: 1
+##              time limit (HH:MM:SS): 04:00:00
+##              Memory: 4gb
+##              run on dmc
 ###############################################
 
 
@@ -31,51 +35,46 @@ module load fastqc/0.10.1
 ##########  Define variables and make directories
 ## Replace the numbers in the brackets with Your specific information
   ## make variable for your ASC ID so the directories are automatically made in YOUR directory
-MyID=team3_finalproj          ## Example: MyID=aubtss
+MyID=team3_finalproj          ##For this project, scripts were run on the ASC in a joint directory with other team projects, hence the specificity for team 3. 
 
-## Make variable that represents YOUR working directory(WD) in scratch, your Raw data directory (DD) and the pre or postcleaned status (CS).
-DD=/scratch/$MyID/PracticeRNAseq/RawData   			## Example: DD=/scratch/$MyID/PracticeRNAseq/RawData
-WD=/scratch/$MyID/PracticeRNAseq				## Example: WD=/scratch/$MyID/PracticeRNAseq
+## Make variables that represent YOUR working directory (WD) and your Raw data directory (DD) in scratch, and the precleaned status directory (CS) to be tar-balled at the end.
+DD=/scratch/$MyID/PracticeRNAseq/RawData                        ## Example: DD=/scratch/$MyID/PracticeRNAseq/RawData
+WD=/scratch/$MyID/PracticeRNAseq                                ## Example: WD=/scratch/$MyID/PracticeRNAseq
 CS=PreCleanQuality
- 
-##  make the directories in SCRATCH for holding the raw data 
+
+##  make the directories in SCRATCH for holding the raw data
 ## -p tells it to make any upper level directories that are not there. Notice how this will also make the WD.
 mkdir -p $DD
-## move to the Data Directory
+## move to the Data Directory, where you will be downloading your SRA sequence files 
 cd $DD
 
 ##########  Download data files from NCBI: SRA using the Run IDs
   ### from SRA use the SRA tool kit - see NCBI website https://www.ncbi.nlm.nih.gov/sra/docs/sradownload/
-	## this downloads the SRA file and converts to fastq
-	## -F 	Defline contains only original sequence name.
-	## -I 	Append read id after spot id as 'accession.spot.readid' on defline.
-	## splits the files into R1 and R2 (forward reads, reverse reads)
+        ## this downloads the SRA file and converts to fastq
+        ## -F   Defline contains only original sequence name.
+        ## -I   Append read id after spot id as 'accession.spot.readid' on defline.
+        ## splits the files into R1 and R2 (forward reads, reverse reads)
 
-## These samples are from Bioproject PRJNA437447. An experiment on Daphnia pulex, 5 samples on ad lib feed, 5 samples on caloric restriction diet
-## https://www.ncbi.nlm.nih.gov/bioproject?LinkName=sra_bioproject&from_uid=5206312
-## For class only do the 6 that you are assigned, delete the other 4 from this list
+## These samples are from Bioproject PRJNA438822. An experiment on Daphnia pulex, looking to see if maternal age has an effect on how offspring respond to 
+##  selenium toxicity with respect to reproduction, lifespan, and resistance to heat-induced stress
+## https://www.ncbi.nlm.nih.gov/bioproject?LinkName=biosample_bioproject&from_uid=8729509
 
 vdb-config --interactive
 
-#fastq-dump -F --split-files SRR6819014
-fastq-dump -F --split-files SRR6819015
-fastq-dump -F --split-files SRR6819016
-#fastq-dump -F --split-files SRR6819017
-#fastq-dump -F --split-files SRR6819018
-fastq-dump -F --split-files SRR6819019
-fastq-dump -F --split-files SRR6819020
-#fastq-dump -F --split-files SRR6819021
-fastq-dump -F --split-files SRR6819022
-fastq-dump -F --split-files SRR6819023
+##The three SRA sequence files shown below are meant to exemplify how to download given SRA sequences of interest. For our project, there were a total of 18 samples, not fully shown here for simplicity's sake.   
+
+fastq-dump -F --split-files SRR6853331 #Sample 13; a pool a three individuals from Experimental Lab Population 2. Their mothers were young (8 days old) when they were born (2nd-3rd brood). The sample individuals were reared in 408 ug of Seleno-L-methionine / L of COMBO media.
+fastq-dump -F --split-files SRR6853332 #Sample 14; a pool a three individuals from Experimental Lab Population 2. Their mothers were young (8 days old) when they were born (2nd-3rd brood). The sample individuals were reared in 102 ug of Seleno-L-methionine / L of COMBO media.
+fastq-dump -F --split-files SRR6853336 #Sample 18; a pool a three individuals from Experimental Lab Population 1. Their mothers were young (8 days old) when they were born (2nd-3rd brood). The sample individuals were reared in 0 ug of Seleno-L-methionine / L of COMBO media.
 
 ##### Extra ####
-## If you are downloaded data from a sequencing company instead of NCBI, using wget for example, then calculate the md5sum values of all the files in the folder (./*), and read into a text file.
-## then you can compare the values in this file with the ones provided by the company.
-md5sum ./* > md5sum.txt
+## If you are downloading data from a sequencing company instead of NCBI, using wget for example, then calculate the md5sum values of all the files in the folder (./*), and read into a text file.
+## then, you can compare the values in this file with the ones provided by the company.
+## md5sum ./* > md5sum.txt
 
 ##### Extra ####
-## If you data comes with multiple R1 and R2 files per individual. You can contatenate them together using "cat" before running FASTQC
-## see examples below for one file. You will probably want to use a loop to process through all the files.
+## If you data comes with multiple R1 and R2 files per individual, you can contatenate them together using "cat" before running FASTQC.
+## See examples below for one file. You will probably want to use a loop to process through all of the files.
 #cat SRR6819014*_R1_*.fastq.gz > SRR6819014_All_R1.fastq.gz
 #cat SRR6819014*_R2_*.fastq.gz > SRR6819014_All_R2.fastq.gz
 
@@ -83,10 +82,12 @@ md5sum ./* > md5sum.txt
 ############## FASTQC to assess quality of the sequence data
 ## FastQC: run on each of the data files that have 'All' to check the quality of the data
 ## The output from this analysis is a folder of results and a zipped file of results
-mkdir $WD/$CS
+## NOTE: Make sure that your $CS directory has already been made before proceeding... or else make it now! 
+
 fastqc *.fastq --outdir=$WD/$CS
 
-#######  Tarball the directory containing the FASTQC results so we can easily bring it back to our computer to evaluate.
-## when finished use scp or rsync to bring the tarballed .gz results file to your computer and open the .html file to evaluate the quality of your raw data.
+#######  Tarball the directory containing the FASTQC results so we can easily bring it back to our computer to evaluate (your CS directory)
 cd $WD/$CS
 tar cvzf $CS.tar.gz $WD/$CS/*
+
+## when finished use scp or rsync to bring the tarballed .gz results file to YOUR computer and open the .html file to evaluate the quality of your raw data.
